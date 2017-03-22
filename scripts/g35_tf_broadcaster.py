3729d190aa24905456f0043a8b7102d0a7a65409
Dan Pierce <jdp0009@auburn.edu>
2017-03-22 11:54:06 -0500

    added functionality to moving wheels

--- scripts/g35_tf_broadcaster.py
+++ scripts/g35_tf_broadcaster.py
@@ -22,13 +22,21 @@ class G35TfBroadcaster:
     # ---- Class variables
     self.reference_initialized = False
     self.ref_lla = (0.,0.,0.)
-    
+
+    self.rot_angle_fl = 0.;
+    self.rot_angle_rl = 0.;
+    self.rot_angle_fr = 0.;
+    self.rot_angle_rr = 0.;
+
     # ---- ROS parameters
     self.marker_frame_id = rospy.get_param('~marker_frame_id','/base_footprint')
     self.a = rospy.get_param('~front_axle_to_cg',0.)
     self.b = rospy.get_param('~rear_axle_to_cg',0.)
+    self.Nsw = rospy.get_param('~steering_wheel_gear_ratio',0.)
+    self.wr = rospy.get_param('~wheel_radius',0.)
     self.t_f = rospy.get_param('~front_track_width',0.)
     self.t_r = rospy.get_param('~rear_track_width',0.)
+    self.ws_dt = rospy.get_param('~wheel_speed_time_step',0.05)
 
     # ---- ROS TF
     self.br = tf.TransformBroadcaster()
@@ -41,6 +49,7 @@ class G35TfBroadcaster:
 
     # ---- ROS subscribers
     sep_sub = rospy.Subscriber("/septentrio/odom",Odometry,self.sep_callback,queue_size=1)
+    ws_sub = rospy.Subscriber("/g35can_node/wheel_speeds",g35can_wheel_speed,self.ws_callback,queue_size=1)
 
   def sep_callback(self,msg):
     stamp_ = rospy.get_rostime()
@@ -62,41 +71,63 @@ class G35TfBroadcaster:
     rsp = self.xyz2enu(xyz=xyz,ref_lla=self.ref_lla)
     sep_pos = (rsp.enu[0],rsp.enu[1],rsp.enu[2])
 
-    """""""""""""""""
-    "" Handle TFs  ""
-    """""""""""""""""
+    """""""""""""""
+    "" Handle TF ""
+    """""""""""""""
     self.br.sendTransform(sep_pos,sep_quat,stamp_,"base_footprint","ned")
 
+  def ws_callback(self,msg):
+    stamp_ = rospy.get_rostime()
+
+    ang_vel = msg.wheel_speed_left_front*(2*np.pi/60)*self.wr # [rad/s]
+    self.rot_angle_fl += ang_vel*self.ws_dt
+
+    ang_vel = msg.wheel_speed_left_rear*(2*np.pi/60)*self.wr # [rad/s]
+    self.rot_angle_rl += ang_vel*self.ws_dt
+
+    ang_vel = msg.wheel_speed_right_front*(2*np.pi/60)*self.wr # [rad/s]
+    self.rot_angle_fr += ang_vel*self.ws_dt
+
+    ang_vel = msg.wheel_speed_right_rear*(2*np.pi/60)*self.wr # [rad/s]
+    self.rot_angle_rr += ang_vel*self.ws_dt
+
+    """""""""""""""
+    "" Handle TF ""
+    """""""""""""""
     q_tire = (0.,0.,0.,1.)
+    pos_z = self.wr
 
     # Front left wheel (at tire contact)
     pos_x = self.a
     pos_y = self.t_f/2.
+    q_tire = e2q(0., self.rot_angle_fl, 0.)
 
-    self.br.sendTransform((pos_x, pos_y, 0.),q_tire,stamp_,
+    self.br.sendTransform((pos_x, pos_y, pos_z),q_tire,stamp_,
                           "wheel_left_front","base_footprint")
 
     # Front right wheel (at tire contact)
     pos_x = self.a
     pos_y = -self.t_f/2.
+    q_tire = e2q(0., self.rot_angle_fr, 0.)
 
-    self.br.sendTransform((pos_x, pos_y, 0.),q_tire,stamp_,
+    self.br.sendTransform((pos_x, pos_y, pos_z),q_tire,stamp_,
                           "wheel_right_front","base_footprint")
 
     # Rear left wheel (at tire contact)
     pos_x = -self.b
     pos_y = self.t_r/2.
+    q_tire = e2q(0., self.rot_angle_rl, 0.)
 
-    self.br.sendTransform((pos_x, pos_y, 0.),(0.,0.,0.,1.),stamp_,
+    self.br.sendTransform((pos_x, pos_y, pos_z),(0.,0.,0.,1.),stamp_,
                           "wheel_left_rear","base_footprint")
 
     # Rear right wheel (at tire contact)
     pos_x = -self.b
     pos_y = -self.t_r/2.
+    q_tire = e2q(0., self.rot_angle_rr, 0.)
 
-    self.br.sendTransform((pos_x, pos_y, 0.),(0.,0.,0.,1.),stamp_,
+    self.br.sendTransform((pos_x, pos_y, pos_z),(0.,0.,0.,1.),stamp_,
                           "wheel_right_rear","base_footprint")
-
 
 def main(args):
   rospy.init_node('g35_tf_broadcaster')
